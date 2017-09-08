d60f8f46348955fa025447441a5b8416c85374e4
alisherkasimovv <alisherkasimov1994@gmail.com>
2017-09-08 15:54:28 +0500

    a + b

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,10 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-	}
+		
+		int a = 5;
+		int b = 6;
+		System.out.println(a + b);
+ 	}
 	
 }
